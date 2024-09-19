b42d9e3d7b1ab7cc2870d87fe4f8e5b676ca48b3
Niyaz Kashapov <cashap.niyaz20@gmail.com>
2024-09-19 19:59:16 +0500

    20.2

--- config/settings.py
+++ config/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': "catalog",
         'USER': 'postgres',
-        'PASSWORD': '291094',
+        'PASSWORD': '123456',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
