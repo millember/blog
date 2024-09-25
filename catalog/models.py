860cfb8281c1639ac7dd325810ea7f93815ec95b
Niyaz Kashapov <cashap.niyaz20@gmail.com>
2024-09-25 16:09:01 +0500

    22.

--- catalog/models.py
+++ catalog/models.py
@@ -2,19 +2,14 @@ from django.db import models
 
 
 # Create your models here.
-
-
-# создаем модель категории как одного ко многим
 class Category(models.Model):
-    # наименование, описание
     name = models.CharField(
-        max_length=100, verbose_name="категория", help_text="Введите категорию продукта"
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование категории",
     )
     description = models.TextField(
-        verbose_name="Описание категории",
-        help_text="Введите описание категории продукта",
-        blank=True,
-        null=True,
+        verbose_name="Описание", help_text="Введите описание категории"
     )
 
     class Meta:
@@ -22,74 +17,64 @@ class Category(models.Model):
         verbose_name_plural = "Категории"
 
     def __str__(self):
-        return f"{self.name} {self.description}"
+        return self.name
 
 
-# создаем модель продуктов как многих к одной категории. Ссылка на категорию
-# создаем класс и наследуемся от models.Model
 class Product(models.Model):
-    # имя, порода, фото, дата рождения
-    # создаем поля и прописываем их параметры
+
     name = models.CharField(
         max_length=100,
-        verbose_name="наименование",
-        help_text="введите наименование продукта",
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
     )
     description = models.TextField(
-        max_length=100, verbose_name="описание", help_text="Введите описание продукта"
+        verbose_name="Описание", help_text="Введите описание продукта"
     )
-    image = models.ImageField(
-        upload_to="catalog/image",
+    photo = models.ImageField(
+        upload_to="catalog/photo",
         blank=True,
         null=True,
-        verbose_name="изображение",
-        help_text="загрузите изображение продукта",
+        verbose_name="Фото",
+        help_text="Загрузите фото продукта",
     )
     category = models.ForeignKey(
         Category,
         on_delete=models.SET_NULL,
         verbose_name="Категория",
-        help_text="введите категорию продукта",
-        blank=True,
+        help_text="Введите категорию продукта",
         null=True,
+        blank=True,
         related_name="products",
     )
-    price = models.PositiveIntegerField(
-        default=0,
+    price = models.FloatField(verbose_name="Цена", help_text="Введите цену продукта")
+    created_at = models.DateField(
+        verbose_name="Дата создания", help_text="Введите дату создания продукта"
     )
-    created_at = models.DateTimeField(
-        auto_now_add=True,
-        editable=False,
-        verbose_name="дата создания",
-        help_text="Укажите дату создания записи в БД",
+    updated_at = models.DateField(
+        verbose_name="Дата последнего изменения",
+        help_text="Введите дату последнего изменения продукта",
+        blank=True,
+        null=True,
     )
-    updated_at = models.DateTimeField(
-        auto_now=True,
-        verbose_name="дата последнего изменения",
-        help_text="Укажите дату изменения записи в БД",
-    )
-
     manufactured_at = models.DateField(
-        verbose_name="Дата производства продукта нашего",
-        help_text="Введите дату производства продукта нашего",
+        verbose_name="Дата производства продукта",
+        help_text="Введите дату производства продукта",
         blank=True,
         null=True,
     )
     views_counter = models.PositiveIntegerField(
         verbose_name="Счетчик просмотров",
         help_text="Укажите количество просмотров",
-        default=0
+        default=0,
     )
 
-    # Класс мета
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
         ordering = ["category", "name"]
 
-    # строковое представление объекта
     def __str__(self):
-        return f"{self.name} {self.category} {self.created_at}"
+        return self.name
 
 
 class Version(models.Model):
@@ -99,7 +84,23 @@ class Version(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        verbose_name="продукт",
+        verbose_name="Версия",
+    )
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование продукта",
+    )
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию продукта",
+        null=True,
+        blank=True,
+        related_name="version_product",
     )
 
     version_num = models.PositiveIntegerField(
@@ -122,12 +123,13 @@ class Version(models.Model):
     active = models.BooleanField(
         verbose_name="Признак текущей версии",
         help_text="Введите признак текущей версии продукта",
-        default=False,
+        default=False
     )
 
     class Meta:
         verbose_name = "Версия продукта"
         verbose_name_plural = "Версии продукта"
+        ordering = ["category", "name"]
 
     def __str__(self):
-        return f"{self.product} {self.version_num} {self.version_name} {self.active}"
+        return self.name
